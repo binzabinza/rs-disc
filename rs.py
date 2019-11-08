6f66d3a64205fb80426dfd533672bd4a48d7e568
Jack <jfbinzer@imail.iu.edu>
2019-11-08 17:05:10 -0600

    rs.py grabs post content and timestamp. save_local.py is being used for dev purposes. delete later

--- rs.py
+++ rs.py
@@ -12,17 +12,13 @@ tree = html.fromstring(page)
 
 #it looks like the body's of the post are themselves html. How do I pull out all the elements grouped by post?
 #lets try looping over each individual post, creating a subtree everytime. Scrape all text as plain, then concatentate.
+# timestamp = tree.xpath('//p[@class="forum-post__time-below"]//text()')
+# print(timestamp)
+
 for i in range(1,11):
-    post_raw = tree.xpath(f'//article[{i}]/div[2]/div/span[1]//text()')
+    post_raw = tree.xpath(f'//article[{i}]//span[@class="forum-post__body"]//text()')
+    timestamp = tree.xpath(f'//article[{i}]//p[@class="forum-post__time-below"]//text()')
     post_text = "\n".join(post_raw)
     print(post_text)
+    print(timestamp)
     print('\n--END--\n')  # there may be a way to clean all this up
-
-
-
-
-
-# message_content = tree.xpath('//span[@class="forum-post__body"]/text()')
-# timestamp = tree.xpath('//p[@class="forum-post__time-below"]/text()')
-# print(message_content)
-# print(timestamp)
