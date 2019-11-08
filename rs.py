3d6f9c70706ccd26993fc7e4ad80a8e76ef604e6
Jack <jfbinzer@imail.iu.edu>
2019-11-08 03:11:09 -0600

    ugh not working

--- rs.py
+++ rs.py
@@ -5,4 +5,7 @@ from lxml import html
 url = "http://services.runescape.com/m=forum/forums.ws?17,18,812,66119561"
 
 page = requests.get(url, headers={'content-type' : 'application/json'})
-print page.content
+tree = html.fromstring(page.content)
+
+message_content = tree.xpath('//span[@class="forum-post__body]/text()"]')
+print message_content
