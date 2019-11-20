1dce8f9cf12dca5e51a4e02d5fb33eb8ea6b14a4
binzabinza <jfbinzer@gmail.com>
2019-11-20 02:03:19 -0600

    broke scrape into separate functions for each data point being scraped, created method to grab number of posts (articles) on page, added methods to get the total number of pages in the thread as well as the current page, __init__() now auto sets current and max_pages and now takes a thread argument (any thread page's url works), store_last_timestamp() saves last scraped information to text file, clear_cache only resets object variables relating to scraping data - it does NOT reinitialize the object

--- rs.py
+++ rs.py
@@ -2,11 +2,16 @@ import requests
 from lxml import html
 
 class RS_Scraper:
-    def __init__(self):
+    def __init__(self, thread):
         #initialize scraping object
+        self.thread = thread
         self.timestamps = []
         self.bodies     = []
         self.usernames  = []
+
+        tmp = self.create_tree(thread)
+        self.current_page = self.get_current_page(tmp)
+        self.max_page     = self.get_max_page(tmp)
 
     def __str__(self):
         #prints formatted content scraped from page
@@ -41,48 +46,97 @@ class RS_Scraper:
         #     inputs  :     type     : required  :  default  :  possible values
         #     tree    :  lxml.tree   :    yes    :           :
         #
-        #This will error for the last page where there are <=10 posts
 
-        usernames_raw = tree.xpath('//a[@class="post-avatar__name-link"]//text()') #TODO: not working
+        self.scrape_usernames(tree)
+        num_posts = self.get_num_posts(tree)
+
+        for i in range(1, num_posts + 1): #1 based indexing because dumb
+            self.scrape_posts(tree, i)
+            self.scrape_timestamps(tree, i)
+
+    ################################
+    #  This section contains all scraping methods. All XPath querys appear here
+    ###############################
+    def get_num_posts(self, tree):
+        #counts the number of posts (article nodes) on a given page
+        cnt = tree.xpath('count(//article)')
+        return int(cnt)
+
+    def scrape_usernames(self, tree):
+        usernames_raw = tree.xpath('//a[@class="post-avatar__name-link"]//text()')
         usernames = list([x.encode('ascii', 'ignore').decode('ascii') for x in usernames_raw])  # don't touch this it will super fuck usernames up
         self.usernames += usernames
-        for i in range(1,11): #TODO: find a simple way to determine how many posts are on a page.
-            #grab content from html tree using XPath Query
-            post_raw      = tree.xpath(f'//article[{i}]//span[@class="forum-post__body"]//text()')
-            timestamp_raw = tree.xpath(f'//article[{i}]//p[@class="forum-post__time-below"]//text()')
 
-            #cleanup data into readable format
-            post_text = "\n".join(post_raw)
-            timestamp_text = " ".join(timestamp_raw)
+    def scrape_posts(self, tree, post_num):
+        post_raw  = tree.xpath(f'//article[{post_num}]//span[@class="forum-post__body"]//text()')
+        post_text = "\n".join(post_raw)
+        self.bodies.append(post_text)
 
-            #add to object attributes
-            self.bodies.append(post_text)
-            self.timestamps.append(timestamp_text)
+    def scrape_timestamps(self, tree, post_num):
+        timestamp_raw = tree.xpath(f'//article[{post_num}]//p[@class="forum-post__time-below"]//text()')
+        timestamp_text = " ".join(timestamp_raw)
+        self.timestamps.append(timestamp_text)
 
-    def store_last_timestamp(self):
-        #TODO: store the last scraped timestamp
-        pass
+    def get_max_page(self, tree):
+        # Determines the total number of pages on the thread
+        # Takes an HTML tree object. Sets and returns the object variable max_page
+        maxp = int(tree.xpath('//a[@class="forum-pagination__top-last"]//text()')[0])
+        self.max_page = maxp
+        return self.max_page
+
+    def get_current_page(self, tree):
+        # Determines the current page number in the thread
+        # Takes an HTML object. Sets and retunrs the object variable current_page
+        curp = int(tree.xpath('//li[@class="current"]/a/text()')[0])
+        self.current_page = curp
+        return self.current_page
+
+    ###################
+    # Miscellaneous Methods
+    ###################
+    def store_last_timestamp(self, url, filename="last_scraped_post.txt"):
+        # this will save the latest timestamp in the cache to a file
+        last_timestamp = self.timestamps[-1]
+        f = open(filename, 'w')
+        f.write(url)
+        f.write('\n')
+        f.write(str(self.current_page))
+        f.write('\n')
+        f.write(last_timestamp)
+        f.close()
+        return last_timestamp
 
     def save_cache(self):
         #TODO: method of saving what has been scraped
-        pass
+        #right now we are simply dumping the object to terminal
+        print(self)
 
     def clear_cache(self):
-        #this will reset the object
-        #probably should be deprecated and roll into save_cache
-        self.__init__()
-
-
+        #this will reset the (cache|buffer) object variables
+        self.timestamps = []
+        self.bodies     = []
+        self.usernames  = []
 
 ##########
 # main debugging
 ##########
 url = "http://services.runescape.com/m=forum/forums.ws?17,18,812,66119561,goto,{}"
 
-s = RS_Scraper()
+s = RS_Scraper(url)
 
-for i in range(1, 5):
-    x = s.create_tree(url.format(i))
-    s.scrape(x)
+# !!CURRENT ISSUE !!! 11/20 1:55AM CT
+# The following page:
+#       http://services.runescape.com/m=forum/forums.ws?17,18,812,66119561,goto,29
+# contains a post that has been (deleted|hidden) and as such, no username is associated
+# This needs to be remedied
+#!!!!!
 
+for i in range(s.current_page, s.max_page + 1): #1 based indexing because dumb
+    page_tree = s.create_tree(url.format(i))
+    s.scrape(page_tree)
+
+    s.save_cache()
+    if i % 40 != 0 : s.clear_cache()
+
+s.store_last_timestamp(url)
 print(s)
