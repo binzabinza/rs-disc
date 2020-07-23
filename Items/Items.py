e8361d83011dcdc770011b1a50bd73528a8ea599
Shane Bielefeld <shanebielefeld@gmail.com>
2020-07-23 02:16:39 -0400

    Added dictionary of tokens with uncleaned names mapped to the corresponding cleaned names

---
+++ Items/Items.py
@@ -0,0 +1,447 @@
+tokens = {
+    "santa paws": "santa paws",
+    "santa paw": "santa paws",
+    #
+    "sameowrai's breastplate": "sameowrai's breastplate",
+    "sameowrais breastplate": "sameowrai's breastplate",
+    "sameowrai breastplate": "sameowrai's breastplate",
+    "sameowrai plate": "sameowrai's breastplate",
+    "samurai cat breastplate": "sameowrai's breastplate",
+    "samurai breastplate": "sameowrai's breastplate",
+    #
+    "sameowrai's helmet": "sameowrai's helmet",
+    "sameowrais helmet": "sameowrai's helmet",
+    "sameowrai helmet": "sameowrai's helmet",
+    "samurai cat helmet": "sameowrai's helmet",
+    "samurai helmet": "sameowrai's helmet",
+    #
+    "samurai cat gauntlet": "sameowrai's gauntlet",
+    "samurai gauntlet": "sameowrai's gauntlet",
+    "sameowrai gauntlet": "sameowrai's gauntlet",
+    "sameowrai's gauntlet": "sameowrai's gauntlet",
+    "sameowrais gauntlet": "sameowrai's gauntlet",
+    "samo gauntlets": "sameowrai's gauntlet",
+    #
+    "sameowrai's faulds": "sameowrai's faulds",
+    "sameowrais faulds": "sameowrai's faulds",
+    "sameowrai faulds": "sameowrai's faulds",
+    "samurai faulds": "sameowrai's faulds",
+    "samurai cat faulds": "sameowrai's faulds",
+    #
+    "sameowrai's cata-nya": "sameowrai's cata-nya",
+    "sameowrais cata-nya": "sameowrai's cata-nya",
+    "cata-nya": "sameowrai's cata-nya",
+    "catanya": "sameowrai's cata-nya",
+    #
+    "cat katanas": "sameowrai's cata-nya",
+    "samurai katana": "sameowrai's cata-nya",
+    "sameowrai katana": "sameowrai's cata-nya",
+    #
+    "sameowrai's bow": "sameowrai's bow",
+    "sameowrai bow": "sameowrai's bow",
+    "samurai bow": "sameowrai's bow",
+    "samurai cat bow": "sameowrai's bow",
+    "samo bow": "sameowrai's bow",
+    #
+    "sameowrai's tabi": "sameowrai's tabi",
+    "sameowrai tabi": "sameowrai's tabi",
+    "samurai cat tabi": "sameowrai's tabi",
+    "samurai tabi": "sameowrai's tabi",
+    "samo tabi": "sameowrai's tabi",
+    #
+    "blue shorthair": "blue shorthair fur",
+    "blue short hair": "blue shorthair fur",
+    #
+    "wreath crown": "wreath crown",
+    #
+    "wreath shield": "wreath shield",
+    #
+    "party fever walk": "party fever walk",
+    "party fever": "party fever walk",
+    "party walk": "party fever walk",
+    #
+    "conga dance": "conga dance",
+    "conga": "conga dance",
+    #
+    "hot sand walk": "hot sand walk",
+    "hot sand": "hot sand walk",
+    #
+    "loved up walk": "loved up walk",
+    "love up walk": "loved up walk",
+    "loved walk": "loved up walk",
+    "loved up token": "loved up walk",
+    #
+    "loved up rest": "loved up rest",
+    "loved rest": "loved up rest",
+    #
+    "plague doctor walk": "plague doctor walk",
+    "doctor walk": "plague doctor walk",
+    "plague": "plague doctor walk",
+    "doc walk": "plague doctor walk",
+    #
+    "assassin walk override": "assassin walk override",
+    "assassin walk": "assassin walk override",
+    "assassins walk": "assassin walk override",
+    #
+    "crown of seasons": "crown of seasons",
+    "crown of season": "crown of seasons",
+    #
+    "cloak of seasons": "cloak of seasons",
+    "cloak of season": "cloak of seasons",
+    "seasons cloak": "cloak of seasons",
+    #
+    "chest of seasons": "chest of seasons",
+    "chest of season": "chest of seasons",
+    "seasons chest": "chest of seasons",
+    #
+    "bottoms of seasons": "bottoms of seasons",
+    "bottoms of season": "bottoms of seasons",
+    "bottom of season": "bottoms of seasons",
+    "seasons bottom": "bottoms of seasons",
+    "season bottom": "bottoms of seasons",
+    "seasons legs": "bottoms of seasons",
+    "legs of season": "bottoms of seasons",
+    #
+    "gloves of season": "gloves of season",
+    "seasons gloves": "gloves of season",
+    #
+    "boots of seasons": "boots of seasons",
+    "boots of season": "boots of seasons",
+    "seasons boots": "boots of seasons",
+    #
+    "cape of season": "cape of season",
+    #
+    "pet of seasons": "pet of seasons",
+    "pet of season": "pet of seasons",
+    #
+    "crab hat": "crab hat",
+    #
+    "christmas jumper token (party hat)": "christmas jumper token (party hat)",
+    "christmas jumper (party hat)": "christmas jumper token (party hat)",
+    "christmas jumper (partyhat)": "christmas jumper token (party hat)",
+    "christmas jumper (phat)": "christmas jumper token (party hat)",
+    "party hat jumper": "christmas jumper token (party hat)",
+    "phat jumper": "christmas jumper token (party hat)",
+    #
+    "christmas jumper (christmas tree)": "christmas jumper (christmas tree)",
+    "christmas tree jumper ": "christmas jumper (christmas tree)",
+    #
+    "christmas jumper (chinchompa)": "christmas jumper (chinchompa)",
+    "christmas jumper token (chinchompa)": "christmas jumper token (chinchompa)",
+    #
+    "rainbow aura": "rainbow aura",
+    #
+    "rainbow gaze": "rainbow gaze",
+    "rb gaze": "rainbow gaze",
+    #
+    "rainbow cape": "rainbow cape",
+    #
+    "rainbow para": "rainbow para",
+    #
+    "rainbow pet": "rainbow pet",
+    #
+    "rainbow halo": "rainbow halo",
+    #
+    "rainbow scythe": "rainbow scythe",
+    #
+    "tree hat": "tree hat",
+    "cth": "tree hat",
+    #
+    "keg companion pet": "keg companion pet",
+    "keg": "keg companion pet",
+    #
+    "gem cape token": "gem cape token",
+    "gem cape": "gem cape token",
+    #
+    "shadow gem melee weapon": "shadow gem melee weapon",
+    #
+    "shadow gem magic weapon": "shadow gem magic weapon",
+    #
+    "shadow gem ranged weapon": "shadow gem ranged weapon",
+    #
+    "shadow gem necklace token": "shadow gem necklace token",
+    "gem necklace": "shadow gem necklace token",
+    #
+    "shadow gem crown token": "shadow gem crown token",
+    "gem crown": "shadow gem crown token",
+    #
+    "shadow gem sack": "shadow gem sack",
+    "shadow sack": "shadow gem sack",
+    "gem sack": "shadow gem sack",
+    #
+    "gem nec": "gem nec",
+    #
+    "wolpertinger pet": "wolpertinger pet",
+    "wolpertinger": "wolpertinger pet",
+    "wolptinger": "wolpertinger pet",
+    #
+    "beach armour override token": "beach armour override token",
+    "beach armour": "beach armour override token",
+    #
+    "clover 2h sword": "clover 2h sword",
+    #
+    "clover staff": "clover staff",
+    #
+    "clover axe": "clover axe",
+    #
+    "clover wand": "clover wand",
+    #
+    "clover crossbow": "clover crossbow",
+    #
+    "clover parasol": "clover parasol",
+    "clover para": "clover parasol",
+    #
+    "mallet token": "mallet token",
+    #
+    "buddy pet": "buddy pet",
+    #
+    "zoltan plushie": "zoltan plushie",
+    #
+    "zoltan pet token": "zoltan pet token",
+    "zoltan pet": "zoltan pet token",
+    #
+    "fish in bag": "fish in bag",
+    "fish in a bag": "fish in bag",
+    #
+    "rubber turkey": "rubber turkey",
+    #
+    "christmas lootbeam": "christmas lootbeam",
+    #
+    "fish mask": "fish mask",
+    #
+    "rei ti ronin tunic": "rei ti ronin tunic",
+    #
+    "rei ti ronin faulds": "rei ti ronin faulds",
+    #
+    "luchador mask": "luchador mask",
+    "luchador": "luchador mask",
+    #
+    "palm tree rest": "palm tree rest",
+    #
+    "stack of presents": "stack of presents",
+    #
+    "stocking tele": "stocking tele",
+    #
+    "present sack": "present sack",
+    "xmas sack": "present sack",
+    #
+    "sombrero": "sombrero",
+    #
+    "hawai'i para": "hawai'i para",
+    "hawaii para": "hawai'i para",
+    "hawai para": "hawai'i para",
+    #
+    "o'ahu parasol": "o'ahu parasol",
+    #
+    "kauai para": "kauai para",
+    #
+    "maui para": "maui para",
+    #
+    "kahului para": "kahului para",
+    #
+    "cadavaberry para": "cadavaberry para",
+    #
+    "orange parasol": "orange parasol",
+    #
+    "easter egg follower": "easter egg follower",
+    "easter egg pet": "easter egg follower",
+    "easter pet": "easter egg follower",
+    "egg pet": "easter egg follower",
+    "eggo pet": "easter egg follower",
+    "eggos pet": "easter egg follower",
+    #
+    "easter egg hat": "easter egg hat",
+    #
+    "dizzy stick teleport token": "dizzy stick teleport token",
+    "dizzy stick": "dizzy stick teleport token",
+    "dizzy tele": "dizzy stick teleport token",
+    #
+    "novtumberfest outfit": "novtumberfest outfit",
+    "novtumber outfit": "novtumberfest outfit",
+    #
+    "novtumberfest dance token": "novtumberfest dance token",
+    "novtumberfest dance": "novtumberfest dance token",
+    #
+    "tribal pet": "tribal pet",
+    #
+    "christmas tree cape": "christmas tree cape",
+    "xmas tree cape": "christmas tree cape",
+    #
+    "snow cape": "snow cape",
+    #
+    "snow parasol": "snow parasol",
+    "snow para": "snow parasol",
+    #
+    "fortune cape": "fortune cape",
+    #
+    "beachball orb": "beachball orb",
+    "beachball": "beachball orb",
+    #
+    "surfboard shield": "surfboard shield",
+    #
+    "seahorse balloon": "seahorse balloon",
+    #
+    "seahorse follower pet": "seahorse follower pet",
+    "seahorse follower": "seahorse follower pet",
+    "seahorse pet": "seahorse follower pet",
+    #
+    "starfish balloon": "starfish balloon",
+    #
+    "pufferfish balloon": "pufferfish balloon",
+    #
+    "pufferfish launcher": "pufferfish launcher",
+    #
+    "pufferfish pet": "pufferfish pet",
+    #
+    "mimic plushie": "mimic plushie",
+    #
+    "mimic tongue cape": "mimic tongue cape",
+    #
+    "meowsketeer's gloves": "meowsketeer's gloves",
+    "meowsketeer gloves": "meowsketeer's gloves",
+    #
+    "meowsketeer's hat": "meowsketeer's hat",
+    #
+    "meowsketeer's boots": "meowsketeer's boots",
+    #
+    "meowsketeer's tunic": "meowsketeer's tunic",
+    #
+    "meowsketeer's rapier": "meowsketeer's rapier",
+    "meow rapier": "meowsketeer's rapier",
+    #
+    "bucket staff": "bucket staff",
+    #
+    "sandy maul": "sandy maul",
+    #
+    "duck ring": "duck ring",
+    #
+    "bone master": "bone master",
+    #
+    "treasure rest": "treasure rest",
+    #
+    "summer prize token": "summer prize token",
+    "prize token": "summer prize token",
+    "summer prize": "summer prize token",
+    #
+    "face paint token": "face paint token",
+    "face paint": "face paint token",
+    #
+    "cocktail flare rest": "cocktail flare rest",
+    #
+    "carrot lance": "carrot lance",
+    #
+    "bloodtusk warlord upper": "bloodtusk warlord upper",
+    "bloodtusk body": "bloodtusk warlord upper",
+    #
+    "bloodtusk warlord lower": "bloodtusk warlord lower",
+    "bloodtusk legs": "bloodtusk warlord lower",
+    #
+    "bloodtusk helmet": "bloodtusk helmet",
+    #
+    "barrel of monkeys": "barrel of monkeys",
+    #
+    "meditation rest": "meditation rest",
+    #
+    "bubble blower": "bubble blower",
+    #
+    "cottontail knight upper body": "cottontail knight upper body",
+    "cottontail upper body": "cottontail knight upper body",
+    "cottontail knight body": "cottontail knight upper body",
+    #
+    "cottontail knight lower body": "cottontail knight lower body",
+    "cottontail lower body": "cottontail knight lower body",
+    #
+    "cottontail knight helm": "cottontail knight helm",
+    "cottontail helm": "cottontail knight helm",
+    #
+    "santa claws": "santa claws",
+    #
+    "hook-a-duck flail": "hook-a-duck flail",
+    "hook-a-duck-flail": "hook-a-duck flail",
+    #
+    "crypt staff": "crypt staff",
+    #
+    "crypt scythe": "crypt scythe",
+    #
+    "crypt shieldbow": "crypt shieldbow",
+    #
+    "glacial shieldbow": "glacial shieldbow",
+    #
+    "fayre dancer": "fayre dancer",
+    #
+    "cerberus claws": "cerberus claws",
+    #
+    "cerberus bow": "cerberus bow",
+    #
+    "squeaker axe token": "squeaker axe token",
+    #
+    "robin": "robin",
+    #
+    "penguin plush": "penguin plush",
+    #
+    "mammoth plush": "mammoth plush",
+    #
+    "apple para": "apple para",
+    #
+    "lemon para": "lemon para",
+    #
+    "lime para": "lime para",
+    #
+    "snowverload plush": "snowverload plush",
+    #
+    "bucking yak": "bucking yak",
+    #
+    "pinata somberero": "pinata somberero",
+    #
+    "pinata plushie": "pinata plushie",
+    #
+    "ocean legs": "ocean legs",
+    #
+    "ocean bow": "ocean bow",
+    #
+    "ocean helm": "ocean helm",
+    #
+    "ocean body": "ocean body",
+    #
+    "ocean xbow": "ocean xbow",
+    #
+    "claudia wings": "claudia wings",
+    #
+    "xmas kite": "xmas kite",
+    #
+    "woolly pudding hat": "woolly pudding hat",
+    #
+    "lifeguard chair token": "lifeguard chair token",
+    #
+    "purified wand": "purified wand",
+    #
+    "purified halberd": "purified halberd",
+    #
+    "purified shortbow": "purified shortbow",
+    #
+    "purified staff": "purified staff",
+    #
+    "purified swords": "purified swords",
+    #
+    "purified greatsword": "purified greatsword",
+    #
+    "purified crossbow": "purified crossbow",
+    #
+    "purified 2h crossbow": "purified 2h crossbow",
+    #
+    "coral dagger": "coral dagger",
+    #
+    "coral sword": "coral sword",
+    #
+    "banana boat mount": "banana boat mount",
+    #
+    "sandy sandwich rest": "sandy sandwich rest",
+    #
+    "goebie backpack": "goebie backpack",
+    #
+    "arthur pet": "arthur pet",
+    #
+    "shovel sword": "shovel sword",
+    #
+    "sand dunk rest": "sand dunk rest"
+}
