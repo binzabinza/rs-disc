2aedc7ea78cb2fa44cad08e201d22bcfd94f09fa
Shane Bielefeld <shanebielefeld@gmail.com>
2020-08-11 23:12:03 -0400

    Added type hints to rs_scraper.py and touched up doc comments

--- rs_scraper.py
+++ rs_scraper.py
@@ -1,9 +1,13 @@
 import requests
 from lxml import html
+from typing import List, Tuple
 
 class RSScraper:
+    """
+        Handles interaction with the forum and parsing html.
+    """
 
-    def __init__(self, thread_url):
+    def __init__(self, thread_url: str):
         #initialize scraping object
         self.thread_url = thread_url  #url of the thread to be scraped
 
@@ -12,7 +16,7 @@ class RSScraper:
         self.current_page = self.get_current_page() #number corresponding to current page
         self.current_post = 1  #post number on the current page, defaults to 1
 
-    def __str__(self):
+    def __str__(self) -> str:
         #prints formatted content scraped from page
         text = ""
         for i, timestamp in enumerate(self.timestamps):
@@ -26,26 +30,32 @@ class RSScraper:
             text += "---\n"
         return text
 
-    def __create_tree__(self, page_num):
+    def __create_tree__(self, page_num: int):
         """
-        creates a tree object for XPath queries and saves it to self.current_tree
-            inputs   :  type  : required  :  default  :  possible values
-            page_num : int    : yes       :           :
+        Creates a tree object for XPath queries and saves it to self.current_tree.
+
+        Parameters
+        ----------
+        page_num : int
+            The page of the forum thread to create the tree for.
         """
         page = requests.get(self.thread_url.format(page_num), headers={'content-type' : 'application/json'})
         tree = html.fromstring(page.content)
         self.current_tree = tree
 
-    def scrape_page(self, page_index, post_index_start=1):
+    def scrape_page(self, page_index: int, post_index_start: int = 1) -> List[Tuple[str, str, str, int, int]]:
         """
-        !!!NOTE: by deafult this method will return the entire page. By passing a post_index_start, it will return less!!!!
+        Scrape content off of the page specified by page_index.
 
-        Scrape content off of the page specified by page_index
+        NOTE: by deafault this method will return the entire page. By passing a post_index_start, it will return less!!!
 
         Parameters
         ----------
         page_index : int
             The page of the forum thread that should be scraped.
+
+        post_index_start : int
+            The index of the first post on the page to start scraping from (1-indexed). Defaults to first post on page.
 
         Returns
         -------
@@ -76,34 +86,34 @@ class RSScraper:
     ################################
     #  This section contains all scraping methods. All XPath querys appear here
     ###############################
-    def get_num_posts(self):
+    def get_num_posts(self) -> int:
         #counts the number of posts (article nodes) on a given page
         cnt = self.current_tree.xpath('count(//article)')
         return int(cnt)
 
-    def scrape_usernames(self):
+    def scrape_usernames(self) -> str:
         usernames_raw = self.current_tree.xpath('//a[@class="post-avatar__name-link"]//text()')
         usernames = list([x.encode('ascii', 'ignore').decode('ascii') for x in usernames_raw])  # don't touch this it will super fuck usernames up
         return usernames
 
-    def scrape_posts(self, post_num):
+    def scrape_posts(self, post_num: int) -> str:
         post_raw  = self.current_tree.xpath(f'//article[{post_num}]//span[@class="forum-post__body"]//text()')
         post_text = "\n".join(post_raw)
         return post_text
 
-    def scrape_timestamps(self, post_num):
+    def scrape_timestamps(self, post_num: int) -> str:
         timestamp_raw = self.current_tree.xpath(f'//article[{post_num}]//p[@class="forum-post__time-below"]//text()')
         timestamp_text = " ".join(timestamp_raw)
         return timestamp_text
 
-    def get_max_page(self):
+    def get_max_page(self) -> int:
         # Determines the total number of pages on the thread
         # Takes an HTML tree object. Sets and returns the object variable max_page
         maxp = int(self.current_tree.xpath('//a[@class="forum-pagination__top-last"]//text()')[0])
         self.max_page = maxp
         return self.max_page
 
-    def get_current_page(self):
+    def get_current_page(self) -> int:
         # Determines the current page number in the thread
         # Takes an HTML object. Sets and retunrs the object variable current_page
         curp = int(self.current_tree.xpath('//li[@class="current"]/a/text()')[0])
